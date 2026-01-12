e51d67a9e2a5a398b48292aaac813f7a23d2038b
Recep Demir <recep.demir@powercoders.org>
2026-01-12 10:37:19 +0100

    Initial Commit

---
+++ WordBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordBar
+//
+//  Created by Recep Demir on 12.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordBar/WordBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordBarApp.swift
+//  WordBar
+//
+//  Created by Recep Demir on 12.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct WordBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
